efa0b41bfa8c3981096acf951ebeba6f5c442e21
El-Guard <alexander.ellegaard@outlook.com>
2026-09-06 16:13:37 -0400

    FCT View Details ZombieType Nombre Zombie et Moyenne Points
    Ajout du nombre de zombies et la moyenne des points pour le Type de zombie dans la View Details.

--- ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
@@ -44,7 +44,9 @@ namespace ZombieParty.Controllers
             ZombieTypeVM zombieTypeVM = new()
             {
                 ZombieType = new(),
-                ZombiesList = _baseDonnees.Zombies.Where(z => z.ZombieTypeId == id).ToList()
+                ZombiesList = _baseDonnees.Zombies.Where(z => z.ZombieTypeId == id).ToList(),
+                ZombiesCount = _baseDonnees.Zombies.Count(),
+                PointsAverage = _baseDonnees.Zombies.Average(z => z.Point)
             };
 
             zombieTypeVM.ZombieType = _baseDonnees.ZombieTypes.FirstOrDefault(zt => zt.Id == id);
